a00392c7f30b01a892c53da25a47e7bea89ee992
Pogoretsky-Dmytro <pogoretsky.d@gmail.com>
2017-11-19 18:42:18 +0200

    basic configuration with react

--- src/app.js
+++ src/app.js
@@ -2,9 +2,15 @@ import css from './app.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import LogoImg from './assets/images/logoW.png';
+
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  	<div>
+	  	<h1>Hello, world!</h1>
+	  	
+	  	
+	</div>,
   document.getElementById('root')
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
 var path = require('path');
+
+var isProd = process.env.NODE_ENV === 'production';// true or false
+var cssDev = ['style-loader', 'css-loader'];
+var cssProd = ExtractTextPlugin.extract({
+				fallback: "style-loader",
+	          	use: "css-loader"
+});
+
+var cssConfig = isProd ? cssProd : cssDev; 
 
 module.exports = {
 	entry: './src/app.js',
@@ -13,26 +23,44 @@ module.exports = {
 	    rules: [
 	      	{
 	        	test: /\.css$/,
-	        	use: ExtractTextPlugin.extract({
-	          		fallback: "style-loader",
-	          		use: "css-loader"
-        		})
+	        	use: cssConfig
       		},
+
       		{
       			test: /\.js$/,
       			exclude: /node_modules/,
       			use: 'babel-loader'
-      		}
-	      // 	{
-	      //   test: /\.css$/,
-	      //   use: [ 'style-loader', 'css-loader' ]
-	      // }
+      		},
+
+		 	{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+						{
+			            	loader: 'file-loader',
+			            	options: {
+			            	name: '[name].[ext]',
+			            	outputPath: 'assets/images/',
+			            	//publicPath: 'assets/images/'
+
+			            			}  
+		          		}, 
+		          		{
+		          			loader: 'image-webpack-loader'	
+		          		}
+				  	]
+			},
+
+			{
+		     	test: /\.(png|jpg|)$/,
+		      	loader: 'url-loader?limit=200000'
+		    }
 	    ]
 	  },
 
   	devServer: {
 		  contentBase: path.join(__dirname, "dist"),
-		  compress: true,
+		  //compress: true,
+		  hot: true,
 		  stats: "errors-only",
 		  open: true
 	},
@@ -48,6 +76,11 @@ module.exports = {
 			      template: './src/index.html'
 			    }),
 
-			new ExtractTextPlugin("styles.css")
+			new ExtractTextPlugin({
+				filename: "styles.css",
+				disable: !isProd
+			}),
+			new webpack.NamedModulesPlugin(),
+     		new webpack.HotModuleReplacementPlugin()
 			]
 }
