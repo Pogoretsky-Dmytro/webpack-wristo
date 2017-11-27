da82ba9d520737c029ff5759853f5c2829fcb80f
Pogoretsky-Dmytro <pogoretsky.d@gmail.com>
2017-11-27 14:33:19 +0200

    add server for deploying to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,8 @@ module.exports = {
 	    rules: [
 	      	{
 	        	test: /\.css$/,
-	        	use: cssConfig
+	        	//use: cssConfig
+	        	use: ['style-loader', 'css-loader']
       		},
 
       		{
@@ -81,7 +82,7 @@ module.exports = {
 
 			new ExtractTextPlugin({
 				filename: "styles.css",
-				disable: !isProd
+				//disable: !isProd
 			}),
 			new webpack.NamedModulesPlugin(),
      		new webpack.HotModuleReplacementPlugin()
