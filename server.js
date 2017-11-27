b399467e754b03450423848c6e4daa55dc0f55bd
Pogoretsky-Dmytro <pogoretsky.d@gmail.com>
2017-11-27 11:57:46 +0200

    add server for deploying to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 3000, function () {
     console.log('server is running on port 3000');
 });
