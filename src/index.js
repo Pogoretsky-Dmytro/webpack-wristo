21fea1537c35b9489382dbbebe5fc3729592e7ac
Pogoretsky-Dmytro <pogoretsky.d@gmail.com>
2017-11-28 11:37:43 +0200

    add config to pacage.json

--- src/index.js
+++ src/index.js
@@ -41,6 +41,7 @@ ReactDOM.render(
 			<Route exact path='/' component={Home}/>
 		 	<Route path='/signup' component={SignUp}/>
 		    <Route path='/login' component={Login}/> 
+		    <Route path='/*' component={NotFound}/>
 		</div>
 	</Router>,
 	document.getElementById('root')
